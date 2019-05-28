b26430ee9bd60e3ea2dd7360e28658c839d4e180
Serhei Grajdean <grajdeanserghei@gmail.com>
2019-05-28 21:27:05 +0300

    Implement in memory View functionality

---
+++ MusicMood/MusicMood/Controllers/ArtistsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MusicMood.Models;
+
+namespace MusicMood.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        private static List<ArtistViewModel> _artists = new List<ArtistViewModel>
+        {
+            new ArtistViewModel
+            {
+                Id = 0,
+                Name = "Metalica",
+                Country = "USA",
+            },
+            new ArtistViewModel
+            {
+                Id = 0,
+                Name = "Beatles",
+                Country = "UK",
+            },
+        };
+
+        public IActionResult Index()
+        {
+            return View(_artists);
+        }
+
+    }
+}
---
+++ MusicMood/MusicMood/Models/ArtistViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MusicMood.Models
+{
+    public class ArtistViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+    }
+}
