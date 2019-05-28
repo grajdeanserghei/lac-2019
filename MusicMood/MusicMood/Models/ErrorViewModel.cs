5d2ae2029772418e45d0dd4b840133aa0f8d5ef5
Serhei Grajdean <grajdeanserghei@gmail.com>
2019-05-28 17:54:37 +0300

    Create Web project from VS template

---
+++ MusicMood/MusicMood/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MusicMood.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
